c4c706422a686a5aa2b568054f3fd04fdf776d5a
ClauYokota <cla.ayumi@gmail.com>
2022-05-25 17:57:38 -0300

    at

--- ATIVIDADES/ATIVIDADE 3/ATIVIDADE3JS.js
+++
@@ -1,6 +0,0 @@
-const divLampada = document.querySelector('div.lampada')
-const botao = document.querySelector('#ligar-desligar')
-
-botao.addEventListener('click', () => {
-    divLampada.classList.toggle('apagado')
-})
