3865087ab9c1bf6e54bd94d24cb7cda212276894
ClauYokota <cla.ayumi@gmail.com>
2022-05-27 17:37:59 -0300

    atual

---
+++ ATIVIDADES/Ativ Grupo/CAPTCHA.js
@@ -0,0 +1,10 @@
+
+let valor1=Math.floor(Math.random()*300)
+
+let valor2=Math.floor(Math.random()*10)
+
+console.log(valor1)
+console.log(valor2)
+
+let v1 = document.getElementById("v1")
+console.log(v1)
